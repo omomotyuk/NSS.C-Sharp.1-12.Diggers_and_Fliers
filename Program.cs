dfc3e25def6b349cbf925ee465547135cfb6160f
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-25 11:21:06 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_12.Diggers_and_Fliers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# NSS.C-Sharp.1-12.Diggers_and_Fliers
